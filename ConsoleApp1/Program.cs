5af6e5a926a0425e3cb29e4911944dec98e5aad7
HigashiKouta <74286299+HigashiKouta@users.noreply.github.com>
2020-12-09 15:24:37 +0900

    リモートでコンフリクト変更

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Visual Studio World!");
+            Console.WriteLine("リモートリポジトリで変更した場所");
         }
     }
 }
