256ce73d930c1533805a33eacec52153959d4c35
HigashiKouta <74286299+HigashiKouta@users.noreply.github.com>
2020-12-09 15:30:32 +0900

    BUG

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("リモートリポジトリで変更した場所");
+            Console.WriteLine("リモートリポジトリで変更した場所ｂ");
         }
     }
 }
